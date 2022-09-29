a48b091f700d935bfa2449e674fa250fb0a2ca3a
fjvm09 <fjvm09@gmail.com>
2022-09-29 17:14:14 -0500

    done task 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temp) { 
+  return Math.round(((temp -32)*5/9)*10.0)/10.0;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return Math.round(((temp*9/5)+32)*10.0)/10.0;
 };
 
 // Do not edit below this line
