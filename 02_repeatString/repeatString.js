83151c2630fbe6fbe50897f246a60cee73b8fd22
fjvm09 <fjvm09@gmail.com>
2022-09-29 15:04:10 -0500

    Done task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let output = ''
+    //let output;
+    if (num < 0){ 
+        output = 'ERROR'; 
+        return output;
+    } else {
+        for(let i = 0; i < num; i++){
+        output += `${string}`;
+        } return output;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
