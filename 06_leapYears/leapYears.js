4b1d9452304908e6433fa23cc71f10f8e4b9d93b
fjvm09 <fjvm09@gmail.com>
2022-09-29 16:53:31 -0500

    done task 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result;
+    if  (year % 100 == 0){
+        result = (year % 400 == 0)
+        return result;
+    } else {
+        result = (year % 4 == 0)
+        return result;
+    }
 };
 
 // Do not edit below this line
