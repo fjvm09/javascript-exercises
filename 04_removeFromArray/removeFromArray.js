b2d7095ab604f632e0ba358fc78a843e7f6133b2
fjvm09 <fjvm09@gmail.com>
2022-09-29 16:10:26 -0500

    DONE TASK 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...quit) {
+   for (const value of quit){
+    const index = array.indexOf(value);
+    if (index > -1){
+        array.splice(index, 1);
+        }
+    } return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
