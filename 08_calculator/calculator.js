a233d7b7123aa0e07453ded3014314b430b320a7
fjvm09 <fjvm09@gmail.com>
2022-10-04 18:23:12 -0500

    Done tasks 08 & 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  if (isNaN(num1)) {num1 = parseInt(0)}
+  if (isNaN(num2)) {num2 = parseInt(0)}
+  let result = num1 + num2;
+	return result
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let result = 0;
+    for(let i = 0; i < x.length; i++){
+      if (x[i] == false){
+        result +=0
+      } result += x[i]
+    } return result;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  let result = 1;
+  for(let i = 0; i < x.length; i++){
+    if (x[i] == false){
+      result *=0
+    } result *= x[i]
+  } return result;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	let result = 1;
+  for (let i = 0; i < y; i++){
+    result *= x
+  } return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = 1;
+  if (x == 0){
+    result = 1;
+  } else {
+    for (let i = 1; i <= x; i++){
+    result *= i;
+  }} return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = string.toLowerCase().split("");
+    console.log(array)
+    let arrayFiltered = array.filter((x) => x !== '!')
+    let arrayFiltered1 = arrayFiltered.filter((x) => x !== ',')
+    let arrayFiltered2 = arrayFiltered1.filter((x) => x !== '.')
+    let arrayFiltered3 = arrayFiltered2.filter((x) => x !== ' ')
+    console.log(arrayFiltered3)
+    let stringFiltered = arrayFiltered3.join("");
+    let stringReversed = arrayFiltered3.reverse().join("");
+    if (stringFiltered == stringReversed){
+        return true;
+    } return false;
 };
 
 // Do not edit below this line
