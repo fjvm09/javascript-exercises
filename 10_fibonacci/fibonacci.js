7e0405205021e11a81d5e8e03aba1302a7b07dc6
fjvm09 <fjvm09@gmail.com>
2022-10-04 18:57:05 -0500

    done task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0){return "OOPS"}
+    let firstPosition = 0
+    let secondPosition = 0
+    let result;
+    for (let i = 1; i <= x; i++){
+        sum = firstPosition + secondPosition;
+        result = sum;
+        firstPosition = secondPosition;
+        secondPosition = sum
+        if (firstPosition == 0 && secondPosition == 0){
+            result = 1
+            secondPosition = 1
+        }
+    } 
+    return result;
 };
 
 // Do not edit below this line
