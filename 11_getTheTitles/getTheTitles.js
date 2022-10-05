15b8cc16a3215698defb4b4f304a861bbfb22e57
fjvm09 <fjvm09@gmail.com>
2022-10-04 19:11:56 -0500

    done task 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let newArray = []
+    array.forEach(item => { 
+        array.find(item => item.title !== null)
+        newArray.push(item.title)
+    }); return newArray
 };
 
 // Do not edit below this line
