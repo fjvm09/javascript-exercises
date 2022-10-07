012b9cd40e414f837473c142a18d66729c5e5d7c
fjvm09 <fjvm09@gmail.com>
2022-10-07 11:51:14 -0500

    done task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort((a, b) => {
+        if (!a.yearOfDeath){a.yearOfDeath = (new Date().getFullYear())}
+        if (!b.yearOfDeath){b.yearOfDeath = (new Date().getFullYear())}
+        let aYears = a.yearOfDeath - a.yearOfBirth;
+        let bYears = b.yearOfDeath - b.yearOfBirth;
+        return aYears > bYears ? -1 : 1;
+    }); console.log(oldest)
+    return oldest[0];
 };
 
 // Do not edit below this line
